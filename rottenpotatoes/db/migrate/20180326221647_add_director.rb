eb8a946bdab27a2f561565892bcbc0524ce0c1f0
EC2 Default User <ec2-user@ip-172-31-29-119.us-east-2.compute.internal>
2018-03-27 02:24:05 +0000

    Final Work

--- rottenpotatoes/db/migrate/20180326221647_add_director.rb
+++ rottenpotatoes/db/migrate/20180326221647_add_director.rb
@@ -1,4 +1,4 @@
-class AddDirectorToMovie < ActiveRecord::Migration
+class AddDirector < ActiveRecord::Migration
   def change
     add_column :movies, :director, :string
   end
--- rottenpotatoes/db/schema.rb
+++ rottenpotatoes/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317172204) do
+ActiveRecord::Schema.define(version: 20180326221647) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
